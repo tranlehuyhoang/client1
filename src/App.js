ff9f59994734bd952fcd0ac4d73273f02aca8753
Huyh Huyhu <2509roblox@gmail.com>
2023-02-28 20:23:19 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import './App.css'
+import AuthPage from "./AuthPage";
+import ChatsPage from "./ChatsPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [user, setUser] = useState(undefined);
+
+  if (!user) {
+    return <AuthPage onAuth={(user) => setUser(user)} />;
+  } else {
+    return <ChatsPage user={user} />;
+  }
 }
 
 export default App;
---
+++ src/ChatsPage.js
@@ -0,0 +1,19 @@
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        'fe6f3ad1-8f5d-400a-8731-bc6d4a9aa8e2',
+        props.user.username,
+        props.user.secret
+    );
+    return <div style={{ height: '100vh', width: '100%' }}>
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+    </div>;
+//     <PrettyChatWindow
+//     projectId="fe6f3ad1-8f5d-400a-8731-bc6d4a9aa8e2"
+//     username={props.user.username}
+//     secret={props.user.secret}
+//     style={{ height: '100%' }}
+// />
+};
+export default ChatsPage;
